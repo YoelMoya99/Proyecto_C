618bb6caa1edc094f0661b62a935140515bb2ecb
YoelMoya99 <yoel.moya.carmona@gmail.com>
2023-03-06 11:05:33 -0600

    Ejercicio 2 Final
    Este deberia ser la ultima version del archivo, no deberia de haber más

--- proyecto2.c
+++ proyecto2.c
@@ -1,10 +1,42 @@
+/*
+
+*** ENCABEZADO DEL DOCUMENTO ***
+
+Estudiantes:
+Yoel Moya Carmona - B75262
+Manuel Zapata Zamora - B47801
+Luis Diego Nuñez Mora - B75473
+
+*** ESTRUCTURA GENERAL DEL CODIGO ***
+
+Se dio la creación independiente de un algoritmo que acomoda los valores
+de mayor a menor en una lista. Este se logro iterando sobre dicha lista
+iniciando por el valor en la posición 'n' y comparando este con n-1. Si
+n-1 es mayor, cambia ambos numeros de lugar y reinicia el contador. El
+'for loop' utilizado termina una vez que no se realice ningun cambio a}
+la lista.
+
+La segunda función creada se dio para imprimir la lista, la cual recibe un
+contador con la cantidad de valores que fueron leidos e ingresados a la lista
+y los imprime uno por uno iniciando por el mayor en la posicion cero.
+
+En el 'main' se da un manejo de exepciones para la cantidad de argumentos con
+'if statements', y para los numeros no validos del archivo, se aprovechó la
+respuesta de la funcion atof() cuando no logra convertir un 'string' a float,
+la cual automaticamente es cero, y cada vez que aparece un cero, se compara el
+string antes de ser convertido con un string de ceros. En dado caso que esta
+comparacion no sea valida, se puede saber que no es un numero valido y se
+levanta una exepcion. No se continua el proceso de ejecución.
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
 
-
+// Algoritmo que acomoda los numeros de mayor a menor:
 void sort_float(long double *lista, int largo){
 
     long double memory1;
@@ -21,7 +53,7 @@ void sort_float(long double *lista, int largo){
     }
 }
 
-
+// funcion que imprime todos los valores ya acomodados de la lista
 void mayor_a_menor(long double *numeros_float,
                    int place_counter){
 
@@ -36,8 +68,7 @@ void mayor_a_menor(long double *numeros_float,
                 */
 
                 printf(
-                    "Posición %d --> %.3Lf\n",
-                    indice,
+                    "%.3Lf\n",
                     numeros_float[float_index]
                 );
                 float_index++;
@@ -57,7 +88,7 @@ int main(int argc, char *argv[]){
     long double numeros_float[80];
     int place_counter = 0;
 
-
+    // Manejo de exepciones para cantidad de argumentos proveidos:
     if (argc != 2){
         printf(
             "Numero de argumentos invalido. "
@@ -77,15 +108,15 @@ int main(int argc, char *argv[]){
         while ((fscanf(fpuntero, "%s", lista)) != EOF){
             fgetc(fpuntero);
 
-            
+            // Asignación del numero a posicion de lista:
             numeros_float[place_counter] = atof(lista);
+
+            // Manejo de exepciones de los numeros del archivo:
             char *purity_test = lista;
-            // char variable = *purity_test;
             char *purity_test2 ="0";
             char *purity_test3 ="0.0";
             char *purity_test4 ="0.00";
             char *purity_test5 ="0.000";
-            char *purity_test6 ="\n";
 
             if (atof(lista) == 0){
 
@@ -107,30 +138,12 @@ int main(int argc, char *argv[]){
                     exit(0);
                 }
             }
-            if (place_counter != 0){
 
-                if (numeros_float[place_counter] ==
-                numeros_float[place_counter-1]){
-
-                    printf(
-                        "Existe una linea invalida en la fila %d\n"
-                        "Ejecución del programa cancelada.\n",
-                        place_counter+1
-                    );
-
-                    exit(0);
-                }
-            }
-
-
+            // Contador de posiciones en la lista:
             place_counter++;
         }
 
         sort_float(numeros_float, place_counter);
-
-        printf(
-            "Lista de los numeros de mayor a menor:\n"
-        );
 
         mayor_a_menor(numeros_float, place_counter);
     }   
